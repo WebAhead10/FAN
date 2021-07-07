fbdf844d9bff8ac5d2db6fc4d3b9d2e445f6a281
Nida Abu Sneineh <nida.abusneineh@gmail.com>
2021-07-07 04:28:54 +0300

    Activate all links & update css

--- server.js
+++ server.js
@@ -5,10 +5,15 @@ const jwt = require("jsonwebtoken");
 const { STATUS_CODES } = require("http");
 const { Script } = require("vm");
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3001;
 const SECRET = "nkA$SD89&&282hd";
-
+const templates = require("./templates");
+const templatesf = require("./templates-f");
+const templatesa = require("./templates-a");
 const server = express();
+
+//posts
+let posts = [{ author: "Nida", title: "Hi", content: "You are My Best Friend" }];
 
 server.use(cookieParser());
 server.use(express.urlencoded());
@@ -95,37 +100,39 @@ function checkAuth(req, res, next) {
 
 server.get("/profiles", checkAuth, (req, res) => {
   const user = req.user;
-  res.send(`<link rel="stylesheet" href="/style.css"> <h1 class="h">Hello ${user.email}</h1>
+  res.send(`<link rel="stylesheet" href="/main-style.css"> <h1 class="h1">Hello ${user.email}</h1>
   <br> 
-  <a href="/profiles/fadi">Fadi's profile</a> <br>
-  <a href="/profiles/adan">Adan's profile</a> <br>
-  <a href="/profiles/nidaa">Nidaa's profile</a> <br> <br>
-  <a href="/log-out">Log out</a>
+  
+  <a class="a" href="/profiles/fadi">Fadi's profile</a> <br><br>
+  <a class="a" href="/profiles/adan">Adan's profile</a> <br><br>
+  <a class="a" href="/profiles/nidaa">Nidaa's profile</a> <br> <br>
+  <a class="a" href="/log-out">Log out</a>
   `);
 });
 
 server.get("/profiles/adan", checkAuth, (req, res) => {
-  res.send(`<link rel="stylesheet" href="/adan-style.css"><body class="nida"><h1 class="h1">Adan's Profile</h1>
+  res.send(`<link rel="stylesheet" href="/adan-style.css"><body class="adan"><h1 class="h1">Adan's Profile</h1>
   <h2 class="h2">Profile Pic</h2><div class="div"> <img class="img"></img>
  
  </div>
+ <a class="a" href="/adan-newPost">New Post</a> <br><br>
 
- <a class="a" href="/adan's-posts">My Posts</a> <br>
+ <a class="a" href="/adan-posts">My Posts</a> <br>
  <br>
-  <a class="a" href="/profiles">Back to profiles</a> <br>
+  <a class="a" href="/profiles">Back to profiles</a> <br><br>
   <a class="a" href="/log-out">Log out</a>
   `);
 });
 
 server.get("/profiles/fadi", checkAuth, (req, res) => {
-  res.send(`<link rel="stylesheet" href="/fadi-style.css"><body class="nida"><h1 class="h1">Fadi's Profile</h1>
-  <h2 class="h2">Profile Pic</h2><div class="div"> <img class="img"></img>
+  res.send(`<link rel="stylesheet" href="/fadi-style.css"><body class="fadi"><h1 class="h1">Fadi's Profile</h1>
+  <h2 class="h2">Profile Pic</h2><div class="div"> <img class="img" ></img>
  
  </div>
-
- <a class="a" href="/fadi's-posts">My Posts</a> <br>
+ <a class="a" href="/fadi-newPost">New Post</a> <br><br>
+ <a class="a" href="/fadi-posts">My Posts</a> <br>
  <br>
-  <a class="a" href="/profiles">Back to profiles</a> <br>
+  <a class="a" href="/profiles">Back to profiles</a> <br><br>
   <a class="a" href="/log-out">Log out</a>
   `);
 });
@@ -137,12 +144,91 @@ server.get("/profiles/nidaa", checkAuth, (req, res) => {
   <h2 class="h2">Profile Pic</h2><div class="div"> <img class="img"></img>
  
  </div>
-
- <a class="a" href="/nidaa's-posts">My Posts</a> <br>
+ <a class="a" href="/nida-newPost">New Post</a> <br>
  <br>
-  <a class="a" href="/profiles">Back to profiles</a> <br>
+ <a class="a" href="/nida-posts">My Posts</a> <br>
+ <br>
+  <a class="a" href="/profiles">Back to profiles</a> <br><br>
   <a class="a" href="/log-out">Log out</a></body>
   `);
+});
+
+//Nida
+//all posts
+server.get("/nida-posts", (req, res) => {
+  const html = templates.allPosts(posts);
+  res.send(html);
+});
+//new post
+server.get("/nida-newPost", (req, res) => {
+  const html = templates.newPost();
+  res.send(html);
+});
+
+server.post("/nida-newPost", (req, res) => {
+  const newPost = req.body;
+  posts.push(newPost);
+  res.redirect("/nida-posts");
+});
+
+//posts :title
+server.get("/nida-posts/:title", (req, res) => {
+  const post = posts.find((p) => p.title === req.params.title);
+  const html = templates.post(post);
+  res.send(html);
+});
+
+// Fadi
+
+//all posts
+server.get("/fadi-posts", (req, res) => {
+  const html = templatesf.allPosts(posts);
+  res.send(html);
+});
+//new post
+server.get("/fadi-newPost", (req, res) => {
+  const html = templatesf.newPost();
+  res.send(html);
+});
+
+server.post("/fadi-newPost", (req, res) => {
+  const newPost = req.body;
+  posts.push(newPost);
+  res.redirect("/fadi-posts");
+});
+
+//posts :title
+server.get("/fadi-posts/:title", (req, res) => {
+  const post = posts.find((p) => p.title === req.params.title);
+  const html = templatesf.post(post);
+  res.send(html);
+});
+
+//Adan
+
+
+//all posts
+server.get("/adan-posts", (req, res) => {
+  const html = templatesa.allPosts(posts);
+  res.send(html);
+});
+//new post
+server.get("/adan-newPost", (req, res) => {
+  const html = templatesa.newPost();
+  res.send(html);
+});
+
+server.post("/adan-newPost", (req, res) => {
+  const newPost = req.body;
+  posts.push(newPost);
+  res.redirect("/adan-posts");
+});
+
+//posts :title
+server.get("/adan-posts/:title", (req, res) => {
+  const post = posts.find((p) => p.title === req.params.title);
+  const html = templatesa.post(post);
+  res.send(html);
 });
 
 server.get("/error", (req, res, next) => {
